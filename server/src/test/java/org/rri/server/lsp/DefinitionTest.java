6284eea0670709c44c24ebf96425f03619e5c93b
Deuqz <ddemon2002@mail.ru>
2022-07-21 19:14:19 +0300

    Add 1 definition test

---
+++ server/src/test/java/org/rri/server/lsp/DefinitionTest.java
@@ -0,0 +1,27 @@
+package org.rri.server.lsp;
+
+import org.eclipse.lsp4j.*;
+import org.junit.Test;
+import org.rri.server.LspPath;
+import org.rri.server.TestUtil;
+
+public class DefinitionTest extends LspServerTestBase {
+  @Override
+  protected String getProjectRelativePath() {
+    return "project";
+  }
+
+  @Test
+  public void definition() {
+    final var filePath = LspPath.fromLocalPath(getProjectPath().resolve("src/Test.java"));
+    final var definitionParams = new DefinitionParams(new TextDocumentIdentifier(filePath.toLspUri()), new Position(3, 4));
+    final var future = server().getTextDocumentService().definition(definitionParams);
+    final var result = TestUtil.getNonBlockingEdt(future, 30000);
+
+    final var targetRange = new Range(new Position(2, 8), new Position(2, 9));
+    final var originalRange = new Range(new Position(3, 4), new Position(3, 5));
+
+    assertEquals(1, result.getRight().size());
+    assertEquals(new LocationLink(filePath.toLspUri(), targetRange, targetRange, originalRange), result.getRight().get(0));
+  }
+}
