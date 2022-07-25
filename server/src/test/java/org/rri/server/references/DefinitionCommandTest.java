4d1f17aecc52def8ce135a6bf0c0ecd6a3945d64
Deuqz <ddemon2002@mail.ru>
2022-07-25 17:19:54 +0300

    Fix test by changed file

--- server/src/test/java/org/rri/server/references/DefinitionCommandTest.java
+++ server/src/test/java/org/rri/server/references/DefinitionCommandTest.java
@@ -15,7 +15,6 @@ import org.rri.server.util.MiscUtil;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 @RunWith(JUnit4.class)
 public class DefinitionCommandTest extends BasePlatformTestCase {
@@ -33,7 +32,7 @@ public class DefinitionCommandTest extends BasePlatformTestCase {
     virtualFile = virtualFile.findChild("DefinitionJava.java");
     assertNotNull(virtualFile);
     final var path = LspPath.fromVirtualFile(virtualFile);
-    checkDefinitions(path, answersJavaDefinition(), Set.of());
+    checkDefinitions(path, answersJavaDefinition());
   }
 
   @Test
@@ -42,17 +41,16 @@ public class DefinitionCommandTest extends BasePlatformTestCase {
     virtualFile = virtualFile.findChild("first.py");
     assertNotNull(virtualFile);
     final var path = LspPath.fromVirtualFile(virtualFile);
-    checkDefinitions(path, answersPythonDefinition(), Set.of(49));
+    checkDefinitions(path, answersPythonDefinition());
   }
 
-  private void checkDefinitions(LspPath path, Map<Integer, LocationLink> answers, Set<Integer> notCheck) {
+  private void checkDefinitions(LspPath path, Map<Integer, LocationLink> answers) {
     final var file = MiscUtil.resolvePsiFile(getProject(), path);
     assertNotNull(file);
     final var doc = MiscUtil.getDocument(file);
     assertNotNull(doc);
-    final int FILE_SIZE = 126;
+    final int FILE_SIZE = 127;
     for (int offset = 0; offset < FILE_SIZE; ++offset) {
-      if (notCheck.contains(offset)) { continue; }
       final var elem = file.findElementAt(offset);
       assertNotNull(elem);
       var start = elem.getTextRange().getStartOffset();
@@ -104,12 +102,12 @@ public class DefinitionCommandTest extends BasePlatformTestCase {
 
   private Map<Integer, LocationLink> answersPythonDefinition() {
     Map<Integer, LocationLink> answers = new HashMap<>();
-    final var func = range(4, 4, 4, 8);
-    final var a = range(7, 0, 7, 1);
+    final var func = range(5, 4, 5, 8);
+    final var a = range(8, 0, 8, 1);
     final var A = range(0, 6, 0, 7);
-    final var aa = range(8, 0, 8, 2);
-    final var b1 = range(11, 0, 11, 2);
-    final var b2 = range(12, 0, 12, 2);
+    final var aa = range(9, 0, 9, 2);
+    final var b1 = range(12, 0, 12, 2);
+    final var b2 = range(13, 0, 13, 2);
     final var second = range(0, 0, 1, 8);
     final var class1 = range(0, 0, 7, 8);
     final var class2 = range(0, 0, 1, 8);
@@ -120,34 +118,34 @@ public class DefinitionCommandTest extends BasePlatformTestCase {
     final var secondUrl = prefix + "second.py";
     final var class1Url = prefix + "class1.py";
     final var class2Url = prefix + "class2.py";
-    answers.put(5, locationLink(secondUrl, second, range(0, 5, 0, 11)));
-    answers.put(11, locationLink(secondUrl, second, range(0, 11, 0, 12)));
-    answers.put(28, locationLink(class1Url, class1, range(1, 7, 1, 13)));
-    answers.put(34, locationLink(class1Url, class1, range(1, 13, 2, 0)));
-    answers.put(42, locationLink(class2Url, class2, range(2, 7, 2, 13)));
-    answers.put(48, locationLink(class2Url, class2, range(2, 13, 4, 0)));
-    answers.put(72, locationLink(firstUrl, a, a));
-    answers.put(73, locationLink(firstUrl, a, range(7, 1, 7, 2)));
-    answers.put(76, locationLink(secondUrl, A, range(7, 4, 7, 5)));
-    answers.put(77, locationLink(secondUrl, A, range(7, 5, 7, 6)));
-    answers.put(80, locationLink(firstUrl, aa, aa));
-    answers.put(82, locationLink(firstUrl, aa, range(8, 2, 8, 3)));
-    answers.put(85, locationLink(firstUrl, a, range(8, 5, 8, 6)));
-    answers.put(86, locationLink(firstUrl, a, range(8, 6, 9, 0)));
-    answers.put(87, locationLink(firstUrl, func, range(9, 0, 9, 4)));
-    answers.put(91, locationLink(firstUrl, func, range(9, 4, 9, 5)));
-    answers.put(95, locationLink(firstUrl, b1, b1));
-    answers.put(97, locationLink(firstUrl, b1, range(11, 2, 11, 3)));
-    answers.put(100, locationLink(class1Url, class1, range(11, 5, 11, 11)));
-    answers.put(106, locationLink(class1Url, class1, range(11, 11, 11, 12)));
-    answers.put(107, locationLink(class1Url, class1B, range(11, 12, 11, 13)));
-    answers.put(108, locationLink(class1Url, class1B, range(11, 13, 11, 14)));
-    answers.put(111, locationLink(firstUrl, b2, b2));
-    answers.put(113, locationLink(firstUrl, b2, range(12, 2, 12, 3)));
-    answers.put(116, locationLink(class2Url, class2, range(12, 5, 12, 11)));
-    answers.put(122, locationLink(class2Url, class2, range(12, 11, 12, 12)));
-    answers.put(123, locationLink(class2Url, class2B, range(12, 12, 12, 13)));
-    answers.put(124, locationLink(class2Url, class2B, range(12, 13, 12, 14)));
+    answers.put(7, locationLink(class1Url, class1, range(0, 7, 0, 13)));
+    answers.put(13, locationLink(class1Url, class1, range(0, 13, 1, 0)));
+    answers.put(21, locationLink(class2Url, class2, range(1, 7, 1, 13)));
+    answers.put(27, locationLink(class2Url, class2, range(1, 13, 2, 0)));
+    answers.put(33, locationLink(secondUrl, second, range(2, 5, 2, 11)));
+    answers.put(39, locationLink(secondUrl, second, range(2, 11, 2, 12)));
+    answers.put(73, locationLink(firstUrl, a, a));
+    answers.put(74, locationLink(firstUrl, a, range(8, 1, 8, 2)));
+    answers.put(77, locationLink(secondUrl, A, range(8, 4, 8, 5)));
+    answers.put(78, locationLink(secondUrl, A, range(8, 5, 8, 6)));
+    answers.put(81, locationLink(firstUrl, aa, aa));
+    answers.put(83, locationLink(firstUrl, aa, range(9, 2, 9, 3)));
+    answers.put(86, locationLink(firstUrl, a, range(9, 5, 9, 6)));
+    answers.put(87, locationLink(firstUrl, a, range(9, 6, 10, 0)));
+    answers.put(88, locationLink(firstUrl, func, range(10, 0, 10, 4)));
+    answers.put(92, locationLink(firstUrl, func, range(10, 4, 10, 5)));
+    answers.put(96, locationLink(firstUrl, b1, b1));
+    answers.put(98, locationLink(firstUrl, b1, range(12, 2, 12, 3)));
+    answers.put(101, locationLink(class1Url, class1, range(12, 5, 12, 11)));
+    answers.put(107, locationLink(class1Url, class1, range(12, 11, 12, 12)));
+    answers.put(108, locationLink(class1Url, class1B, range(12, 12, 12, 13)));
+    answers.put(109, locationLink(class1Url, class1B, range(12, 13, 12, 14)));
+    answers.put(112, locationLink(firstUrl, b2, b2));
+    answers.put(114, locationLink(firstUrl, b2, range(13, 2, 13, 3)));
+    answers.put(117, locationLink(class2Url, class2, range(13, 5, 13, 11)));
+    answers.put(123, locationLink(class2Url, class2, range(13, 11, 13, 12)));
+    answers.put(124, locationLink(class2Url, class2B, range(13, 12, 13, 13)));
+    answers.put(125, locationLink(class2Url, class2B, range(13, 13, 13, 14)));
     return answers;
   }
 
