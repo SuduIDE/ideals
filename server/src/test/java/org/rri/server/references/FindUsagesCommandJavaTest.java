24029aab2351ce2949257958c27ae2280aa13587
Deuqz <ddemon2002@mail.ru>
2022-07-26 13:46:09 +0300

    Add FindUsages tests for java

---
+++ server/src/test/java/org/rri/server/references/FindUsagesCommandJavaTest.java
@@ -0,0 +1,124 @@
+package org.rri.server.references;
+
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.rri.server.LspPath;
+import org.rri.server.TestUtil;
+
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RunWith(JUnit4.class)
+public class FindUsagesCommandJavaTest extends BasePlatformTestCase {
+
+  @Override
+  protected String getTestDataPath() {
+    return Paths.get("test-data/references").toAbsolutePath().toString();
+  }
+
+  private VirtualFile projectFile;
+
+  @Before
+  public void copyDirectoryToProject() {
+    projectFile = myFixture.copyDirectoryToProject("java/project1/src", "");
+  }
+
+  @Test
+  public void testFindUsagesJavaVariable() {
+    final var file = projectFile.findChild("FindUsagesJavaVariable.java");
+    assertNotNull(file);
+    final var path = LspPath.fromVirtualFile(file);
+    final var findUsagesJavaVariableUri = path.toLspUri();
+
+    final var answers = new HashSet<>(List.of(
+            location(findUsagesJavaVariableUri, range(3, 8, 3, 9)),
+            location(findUsagesJavaVariableUri, range(7, 8, 7, 14)),
+            location(findUsagesJavaVariableUri, range(11, 15, 11, 16))));
+
+    final var positions = List.of(new Position(1, 16), new Position(3, 8));
+    for (final var pos : positions) {
+      check(answers, pos, path);
+    }
+  }
+
+  @Test
+  public void testFindUsagesJavaMethod() {
+    var virtualFile = projectFile.findChild("org");
+    assertNotNull(virtualFile);
+    final var anotherFile = virtualFile.findChild("Another.java");
+    assertNotNull(anotherFile);
+    final var definitionFile = virtualFile.findChild("DefinitionJava.java");
+    assertNotNull(definitionFile);
+    final var anotherPath = LspPath.fromVirtualFile(anotherFile);
+    final var definitionPath = LspPath.fromVirtualFile(definitionFile);
+
+    final var anotherUri = anotherPath.toLspUri();
+    final var definitionUri = definitionPath.toLspUri();
+
+    final var answers = new HashSet<>(List.of(
+            location(anotherUri, range(11, 8, 11, 26)),
+            location(definitionUri, range(11, 8, 11, 11))));
+
+    check(answers, new Position(11, 23), anotherPath);
+    check(answers, new Position(5, 23), definitionPath);
+  }
+
+  @Test
+  public void testFindUsagesJavaClass() {
+    var virtualFile = projectFile.findChild("org");
+    assertNotNull(virtualFile);
+    final var anotherFile = virtualFile.findChild("Another.java");
+    assertNotNull(anotherFile);
+    final var definitionFile = virtualFile.findChild("DefinitionJava.java");
+    assertNotNull(definitionFile);
+    final var anotherPath = LspPath.fromVirtualFile(anotherFile);
+    final var definitionPath = LspPath.fromVirtualFile(definitionFile);
+
+    final var prefix = "temp:///src/";
+    final var definitionJavaUri = prefix + "org/DefinitionJava.java";
+    final var typeDefinitionIntegratingTestUri = prefix + "TypeDefinitionIntegratingTest.java";
+    final var typeDefinitionJavaUri = prefix + "TypeDefinitionJava.java";
+
+    final var answers = new HashSet<>(List.of(
+            location(definitionJavaUri, range(13, 8, 13, 19)),
+            location(definitionJavaUri, range(13, 28, 13, 39)),
+            location(typeDefinitionIntegratingTestUri, range(0, 7, 0, 18)),
+            location(typeDefinitionIntegratingTestUri, range(4, 4, 4, 11)),
+            location(typeDefinitionIntegratingTestUri, range(4, 20, 4, 27)),
+            location(typeDefinitionIntegratingTestUri, range(5, 4, 5, 11)),
+            location(typeDefinitionJavaUri, range(0, 7, 0, 18)),
+            location(typeDefinitionJavaUri, range(4, 8, 4, 15)),
+            location(typeDefinitionJavaUri, range(5, 8, 5, 19))));
+
+    check(answers, new Position(13, 12), definitionPath);
+    check(answers, new Position(2, 13), anotherPath);
+  }
+
+  private void check(Set<Location> answers, Position pos, LspPath path) {
+    final var future = new FindUsagesCommand(pos).runAsync(getProject(), path);
+    final var lst = TestUtil.getNonBlockingEdt(future, 50000);
+    assertNotNull(lst);
+    final var result = new HashSet<>(lst);
+    assertEquals(answers, result);
+  }
+
+  private Location location(String uri, Range targetRange) {
+    return new Location(uri, targetRange);
+  }
+
+  @SuppressWarnings("SameParameterValue")
+  @NotNull
+  private static Range range(int line1, int char1, int line2, int char2) {
+    return new Range(new Position(line1, char1), new Position(line2, char2));
+  }
+}
---
+++ server/test-data/references/java/project1/src/FindUsagesJavaVariable.java
@@ -0,0 +1,14 @@
+public class FindUsagesJavaVariable {
+    private int x;
+    public FindUsagesJavaVariable() {
+        x = 1;
+    }
+
+    public void set(int x) {
+        this.x = x;
+    }
+
+    public int get() {
+        return x;
+    }
+}
