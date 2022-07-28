cc752a3b35158506820dc1b5f6924c444664bf23
Deuqz <ddemon2002@mail.ru>
2022-07-28 15:43:03 +0300

    Add .idea files for test work

--- server/test-data/references/java/project1/src/TypeDefinitionIntegratingTest.java
+++ server/test-data/references/java/project1/src/TypeDefinitionIntegratingTest.java
@@ -1,6 +1,12 @@
-import org.Another;
-
 class TypeDefinitionIntegratingTest {
+  private class Another {
+    int x, y;
+    public Another(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
   public TypeDefinitionIntegratingTest() {
     Another a = new Another(1, 2);
     Another b = a;
