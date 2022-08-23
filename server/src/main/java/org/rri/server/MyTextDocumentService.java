df70b2e148c3c0e2537d087b3787ceae37f0e26a
Sergey.Anchipolevsky <serganch@gmail.com>
2022-08-23 16:25:20 +0300

    fixing erroneous merge after rebase

--- server/src/main/java/org/rri/server/MyTextDocumentService.java
+++ server/src/main/java/org/rri/server/MyTextDocumentService.java
@@ -128,33 +128,6 @@ public class MyTextDocumentService implements TextDocumentService {
     });
   }
 
-  @SuppressWarnings("deprecation")
-  @Override
-  public CompletableFuture<List<Either<SymbolInformation, DocumentSymbol>>> documentSymbol(DocumentSymbolParams params) {
-    return new DocumentSymbolCommand().runAsync(session.getProject(), LspPath.fromLspUri(params.getTextDocument().getUri()));
-  }
-
-  @Override
-  public CompletableFuture<List<Either<Command, CodeAction>>> codeAction(CodeActionParams params) {
-//    LOG.warn("codeAction invoked: " + params);
-    return CompletableFuture.completedFuture(
-        diagnostics().getCodeActions(
-            LspPath.fromLspUri(params.getTextDocument().getUri()),
-            params.getRange()
-        ).stream().map((Function<CodeAction, Either<Command, CodeAction>>) Either::forRight).collect(Collectors.toList())
-    );
-  }
-
-
-  @Override
-  public CompletableFuture<CodeAction> resolveCodeAction(CodeAction unresolved) {
-    return CompletableFuture.supplyAsync(() -> {
-      var edit = diagnostics().applyCodeAction(unresolved);
-      unresolved.setEdit(edit);
-      return unresolved;
-    });
-  }
-
   public void refreshDiagnostics() {
     LOG.info("Start refreshing diagnostics for all opened documents");
     documents().forEach(diagnostics()::launchDiagnostics);
