1546515cbaadf5e4921ad8006e989ca3dd12e977
Deuqz <ddemon2002@mail.ru>
2022-09-09 15:54:01 +0300

    Add failing test. Update hierarchy

--- server/src/test/java/org/rri/ideals/server/rename/RenameCommandJavaTest.java
+++ server/src/test/java/org/rri/ideals/server/rename/RenameCommandJavaTest.java
@@ -1,37 +1,20 @@
 package org.rri.ideals.server.rename;
 
-import com.intellij.testFramework.fixtures.BasePlatformTestCase;
-import org.eclipse.lsp4j.*;
-import org.eclipse.lsp4j.jsonrpc.messages.Either;
-import org.jetbrains.annotations.NotNull;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.RenameFile;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.rri.ideals.server.LspPath;
-import org.rri.ideals.server.TestUtil;
 
 import java.nio.file.Paths;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.rri.ideals.server.TestUtil.newTextEdit;
 
 @RunWith(JUnit4.class)
-public class RenameCommandJavaTest extends BasePlatformTestCase {
-  @NotNull
-  private LspPath renameTestPath;
-  @NotNull
-  private String renameTestUri;
-  @NotNull
-  private String orgTestClassUri;
-
-  @NotNull
-  private static final Comparator<TextEdit> comparator = Comparator
-      .<TextEdit>comparingInt(edit -> edit.getRange().getStart().getLine())
-      .thenComparingInt(edit -> edit.getRange().getStart().getCharacter());
-
+public class RenameCommandJavaTest extends RenameCommandTestBase {
   @Override
   protected String getTestDataPath() {
     return Paths.get("test-data/rename").toAbsolutePath().toString();
@@ -88,23 +71,16 @@ public class RenameCommandJavaTest extends BasePlatformTestCase {
   public void testRenameClass() {
     final var newName = "Inner111";
 
-    final var inner1ClsTextEdit = newTextEdit(5, 14, 5, 20, newName);
-    final var inner1CnstrTextEdit = newTextEdit(5, 41, 5, 47, newName);
-    final var nestedInner1ClsTextEdit = newTextEdit(6, 14, 6, 20, newName);
-    final var nestedInner1CnstrTextEdit = newTextEdit(6, 48, 6, 54, newName);
-
-    final var orgTestClassEdits = textDocumentEdit(orgTestClassUri, list(
-        newTextEdit(7, 22, 7, 28, newName),
-        newTextEdit(9, 11, 9, 17, newName)
-    ));
-
-    var answer = List.of(
-        orgTestClassEdits,
+    final var answer = List.of(
+        textDocumentEdit(orgTestClassUri, list(
+            newTextEdit(7, 22, 7, 28, newName),
+            newTextEdit(9, 11, 9, 17, newName)
+        )),
         textDocumentEdit(renameTestUri, list(
-            inner1ClsTextEdit,
-            inner1CnstrTextEdit,
-            nestedInner1ClsTextEdit,
-            nestedInner1CnstrTextEdit
+            newTextEdit(5, 14, 5, 20, newName),
+            newTextEdit(5, 41, 5, 47, newName),
+            newTextEdit(6, 14, 6, 20, newName),
+            newTextEdit(6, 48, 6, 54, newName)
         ))
     );
 
@@ -114,32 +90,30 @@ public class RenameCommandJavaTest extends BasePlatformTestCase {
     pos = new Position(5, 42);
     checkRename(answer, pos, newName);
   }
+  
+  @Test
+  public void testRenameClassWithFileEdit() {
+    final var newName = "TestClassNext";
 
-  @NotNull
-  private TextDocumentEdit textDocumentEdit(@NotNull String uri, @NotNull List<@NotNull TextEdit> edits) {
-    return new TextDocumentEdit(new VersionedTextDocumentIdentifier(uri, 1), edits);
-  }
+    final var documentEdits = List.of(
+        textDocumentEdit(orgTestClassUri, list(
+            newTextEdit(6, 13, 6, 22, newName),
+            newTextEdit(21, 9, 21, 18, newName)
+        )),
+        textDocumentEdit(renameTestUri, list(
+            newTextEdit(0, 11, 0, 20, newName),
+            newTextEdit(4, 4, 4, 13, newName),
+            newTextEdit(4, 25, 4, 34, newName),
+            newTextEdit(5, 4, 5, 13, newName),
+            newTextEdit(5, 31, 5, 40, newName),
+            newTextEdit(6, 4, 6, 13, newName),
+            newTextEdit(6, 38, 6, 47, newName)
+        ))
+    );
 
-  // RenameCommand returns ListN, but List.of can be List12 => Comparison Failure
-  @NotNull
-  private List<@NotNull TextEdit> list(@NotNull TextEdit... textEdits) {
-    return Stream.of(textEdits).toList();
-  }
+    final var operations = List.of(new RenameFile(orgTestClassUri, orgTestClassUri.replace("TestClass", newName)));
 
-  private void checkRename(@NotNull List<@NotNull TextDocumentEdit> edits, @NotNull Position pos, @NotNull String newName) {
-    final var answer = new WorkspaceEdit(edits.stream().map(Either::<TextDocumentEdit, ResourceOperation>forLeft).toList());
-
-    final var future = new RenameCommand(pos, newName).runAsync(getProject(), renameTestPath);
-    final var result = TestUtil.getNonBlockingEdt(future, 50000);
-
-    assertNotNull(result);
-
-    result.getDocumentChanges().forEach(either -> {
-      final var docEdit = either.getLeft();
-      final var sortedEdits = docEdit.getEdits().stream().sorted(comparator).toList();
-      docEdit.setEdits(sortedEdits);
-    });
-
-    assertEquals(answer, result);
+    final var pos = new Position(4, 5);
+    checkRename(documentEdits, pos, newName, operations);
   }
 }
