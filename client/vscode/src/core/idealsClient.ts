70069d4a8057fac1424c267071d0f448106579f0
rmzs0711 <rmzs0711@yandex.ru>
2022-11-29 14:01:29 +0300

    run by stdio

--- client/vscode/src/core/idealsClient.ts
+++ client/vscode/src/core/idealsClient.ts
@@ -6,6 +6,9 @@ import * as net from 'net';
 import {LanguageClientOptions, RevealOutputChannelOn,} from "vscode-languageclient";
 
 import {LanguageClient, ServerOptions, State, StreamInfo,} from "vscode-languageclient/node";
+import path = require("path");
+import fs = require("fs");
+import os = require('node:os');
 
 const outputChannel = vscode.window.createOutputChannel("IdeaLS Client");
 
@@ -62,11 +65,10 @@ export class IdealsClient {
 
   //Create a command to be run to start the LS java process.
   getServerOptions() {
-    let transportEnv = process.env.IDEALS_TRANSPORT;
+    let transportEnv : string | undefined = vscode.workspace.getConfiguration('ideals').get('startup.transport');
 
     if (transportEnv?.toLowerCase() === "tcp") {
       // Connect to language server via socket
-
       let portEnv = process.env.IDEALS_TCP_PORT;
 
       let connectionInfo = {
@@ -89,16 +91,39 @@ export class IdealsClient {
       };
     }
 
-    let ideaExecutablePath = process.env.IDEALS_IJ_PATH;
+    let ideaExecutablePath : string | undefined = vscode.workspace.getConfiguration('ideals').get('startup.ideaExecutablePath');
 
     if (!ideaExecutablePath) {
       throw new Error("Path to IntelliJ IDEA executable must be specified in environment variable IDEALS_IJ_PATH");
     }
+    let isToolBox = vscode.workspace.getConfiguration('ideals').get('startup.isIdeaInstalledViaToolBox');
+    var content : string;
+    var vmoptionsPath : string; 
+    if (isToolBox) {
+      var ideaVersionDir = path.normalize(path.dirname(path.dirname(ideaExecutablePath)));
+      var ideaVersion = path.basename(ideaVersionDir);
+      var dirWithVmOptions = path.dirname(ideaVersionDir);
+      vmoptionsPath = path.join(dirWithVmOptions, path.basename(ideaVersion) + ".vmoptions");
+    } else {
+      var parent = path.normalize(path.dirname(ideaExecutablePath));
+      vmoptionsPath = path.join(parent, path.basename(ideaExecutablePath) + ".vmoptions");
+    }
+    content = fs.readFileSync(vmoptionsPath).toString();
+    content += "\n-Djava.awt.headless=true"; 
+
+    const tmpdir = os.tmpdir();
+    const finalPath = path.join(tmpdir, String(process.pid) + ".vmoptions");
+    fs.writeFileSync(finalPath, content);
+    
 
     let serverOptions: ServerOptions = {
       command: ideaExecutablePath,
       args: ["lsp-server"],
-      options: {},
+      options: {
+        env: {
+          IDEA_VM_OPTIONS: finalPath,
+        }
+      },
     };
     return serverOptions;
 
