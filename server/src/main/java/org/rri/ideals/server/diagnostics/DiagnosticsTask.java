4a48e3f567488ba37a2e08a5a9967510b4d02f2f
rmzs0711 <rmzs0711@yandex.ru>
2022-12-06 16:42:05 +0300

    remove deprecated methods

--- server/src/main/java/org/rri/ideals/server/diagnostics/DiagnosticsTask.java
+++ server/src/main/java/org/rri/ideals/server/diagnostics/DiagnosticsTask.java
@@ -5,13 +5,11 @@ import com.intellij.codeInsight.daemon.impl.DaemonProgressIndicator;
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInsight.daemon.impl.HighlightingSessionImpl;
 import com.intellij.lang.annotation.HighlightSeverity;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.IndexNotReadyException;
-import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.ProperTextRange;
 import com.intellij.psi.PsiFile;
 import org.eclipse.lsp4j.*;
@@ -23,10 +21,7 @@ import org.rri.ideals.server.LspPath;
 import org.rri.ideals.server.util.Metrics;
 import org.rri.ideals.server.util.MiscUtil;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 class DiagnosticsTask implements Runnable {
@@ -62,12 +57,12 @@ class DiagnosticsTask implements Runnable {
       return null;
 
     final var range = MiscUtil.getRange(doc, info);
-
-    if(info.quickFixActionRanges != null) {
-      registry.registerQuickFixes(
-          range,
-          info.quickFixActionRanges.stream().map(it -> it.first).collect(Collectors.toList()));
-    }
+    var descriptors = new ArrayList<HighlightInfo.IntentionActionDescriptor>();
+    info.findRegisteredQuickFix((intentionActionDescriptor, textRange) -> {
+      descriptors.add(intentionActionDescriptor);
+      return null;
+    });
+    registry.registerQuickFixes(range, descriptors);
 
     return new Diagnostic(range, info.getDescription(), diagnosticSeverity(info.getSeverity()), "ideals");
   }
@@ -98,24 +93,17 @@ class DiagnosticsTask implements Runnable {
 
   @NotNull
   private List<HighlightInfo> getHighlights(@NotNull PsiFile file, @NotNull Document doc) {
-    var disposable = Disposer.newDisposable();
-    try {
-      return Metrics.call(
-          () -> "Analyzing file: " + file.getVirtualFile(),
-          () -> doHighlighting(disposable, doc, file)
-      );
-    } finally {
-      Disposer.dispose(disposable);
-    }
+    return Metrics.call(
+        () -> "Analyzing file: " + file.getVirtualFile(),
+        () -> doHighlighting(doc, file)
+    );
   }
 
+  @SuppressWarnings("UnstableApiUsage")
   @NotNull
-  private List<HighlightInfo> doHighlighting(@NotNull Disposable context,
-                                             @NotNull Document doc, @NotNull PsiFile psiFile) {
+  private List<HighlightInfo> doHighlighting(@NotNull Document doc, @NotNull PsiFile psiFile) {
 
     var progress = new DaemonProgressIndicator();
-
-//    Disposer.register(context, progress);
 
     var project = psiFile.getProject();
 
@@ -128,10 +116,7 @@ class DiagnosticsTask implements Runnable {
 
         // this shouldn't be needed but for some reason the next call fails without it
         HighlightingSessionImpl.runInsideHighlightingSession(psiFile, null, range, false, () -> {
-
         });
-//        HighlightingSessionImpl.runInsideHighlightingSession(psiFile, progress, null, range, false, () -> {
-//        });
 
         final var result = DaemonCodeAnalyzerEx.getInstanceEx(project).runMainPasses(psiFile, doc, progress);
         if (LOG.isTraceEnabled()) LOG.trace("Analyzing file: produced items: " + result.size());
