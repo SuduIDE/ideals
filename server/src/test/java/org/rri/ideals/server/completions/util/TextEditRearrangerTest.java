efe253ba179198bdb24de369e19a0b458f376569
rmzs0711 <rmzs0711@yandex.ru>
2023-01-19 19:10:38 +0600

    new arranger tests

--- server/src/test/java/org/rri/ideals/server/completions/util/TextEditRearrangerTest.java
+++ server/src/test/java/org/rri/ideals/server/completions/util/TextEditRearrangerTest.java
@@ -1,13 +1,16 @@
 package org.rri.ideals.server.completions.util;
 
+import com.intellij.openapi.util.TextRange;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -16,107 +19,128 @@ import java.util.stream.Stream;
 public class TextEditRearrangerTest {
   public static Stream<Arguments> provideAllCasesForMerge() {
     return Stream.of(
-        // |__| [__] !
+        // |__| [!_!]
         Arguments.of(
             0, 1, // start, end
-            4, // caret
-            List.of(new TextEditWithOffsets(2, 3, "_")), // textEdits
-            "01_3$0", // merged string
-            Set.of(new TextEditWithOffsets(1, 4, "")) // expected additional edits
+            new TextRange(2, 3), // caret
+            List.of(new TextEditWithOffsets(2, 5, "___")), // textEdits
+            "01___", // merged string
+            Set.of(new TextEditWithOffsets(1, 5, "")) // expected additional edits
         ),
-        // |__| [__] ! [__]
+        // |__| [!_] [_!]
         Arguments.of(
             0, 1, // start, end
-            4, // caret
+            new TextRange(2, 6), // caret
             List.of(
                 new TextEditWithOffsets(2, 3, "_"),
                 new TextEditWithOffsets(5, 6, "_")
             ), // textEdits
-            "01_3$0", // merged string
+            "01_34_", // merged string
             Set.of(
-                new TextEditWithOffsets(1, 4, ""),
-                new TextEditWithOffsets(5, 6, "_")) // expected additional edits
+                new TextEditWithOffsets(1, 6, "")) // expected additional edits
         ),
-        // |__| [_!]
+        // |__| [!_] [__] [_!]
         Arguments.of(
             0, 1, // start, end
-            3, // caret
-            List.of(new TextEditWithOffsets(2, 4, "___")), // textEdits
-            "01_$0__", // merged string
-            Set.of(new TextEditWithOffsets(1, 4, "")) // expected additional edits
+            new TextRange(2, 7), // caret
+            List.of(
+                new TextEditWithOffsets(2, 3, "_"),
+                new TextEditWithOffsets(4, 5, "_"),
+                new TextEditWithOffsets(6, 7, "_")
+            ), // textEdits
+            "01_3_5_", // merged string
+            Set.of(new TextEditWithOffsets(1, 7, "")) // expected additional edits
         ),
-        // |__| ! [__]
+        // |!_| [_!]
         Arguments.of(
             0, 1, // start, end
-            2, // caret
+            new TextRange(0, 4), // caret
             List.of(new TextEditWithOffsets(3, 4, "_")), // textEdits
-            "01$0", // merged string
+            "012_", // merged string
             Set.of(
-                new TextEditWithOffsets(1, 2, ""),
-                new TextEditWithOffsets(3, 4, "_")) // expected additional edits
+                new TextEditWithOffsets(1, 4, "")) // expected additional edits
         ),
-        // [__] |__| !
+        // [!_] |_!|
         Arguments.of(
             2, 3, // start, end
-            4, // caret
+            new TextRange(0, 3), // caret
             List.of(new TextEditWithOffsets(0, 1, "_")), // textEdits
-            "23$0", // merged string
+            "_12", // merged string
             Set.of(
-                new TextEditWithOffsets(0, 1, "_"),
-                new TextEditWithOffsets(3, 4, "")) // expected additional edits
+                new TextEditWithOffsets(0, 2, "")) // expected additional edits
         ),
-        // [__] ! |__|
+        // [|!_!|] [__]
         Arguments.of(
-            3, 4, // start, end
-            4, // caret
-            List.of(new TextEditWithOffsets(0, 1, "___")), // textEdits
-            "$023", // merged string
-            Set.of(
-                new TextEditWithOffsets(0, 1, "___"),
-                new TextEditWithOffsets(2, 3, "")) // expected additional edits
-        ),
-        // ! [__] |__|
-        Arguments.of(
-            3, 4, // start, end
-            0, // caret
-            List.of(new TextEditWithOffsets(1, 2, "")), // textEdits
-            "$0023", // merged string
-            Set.of(
-                new TextEditWithOffsets(0, 3, "")) // expected additional edits
-        ),
-        // [!_] |__|
-        Arguments.of(
-            3, 4, // start, end
-            2, // caret
-            List.of(new TextEditWithOffsets(1, 2, "___")), // textEdits
-            "_$0__23", // merged string
-            Set.of(
-                new TextEditWithOffsets(1, 3, "")) // expected additional edits
-        ),
-        // [__|]__[|__] !
-        Arguments.of(
-            3, 5, // start, end
-            7, // caret
+            0, 1, // start, end
+            new TextRange(0, 1), // caret
             List.of(
-                new TextEditWithOffsets(2, 4, "___"),
-                new TextEditWithOffsets(4, 6, "")
-            ), // textEdits
-            "___67$0", // merged string
+                new TextEditWithOffsets(0, 1, "_"),
+                new TextEditWithOffsets(3, 4, "_")
+                ), // textEdits
+            "_", // merged string
+            Set.of(
+                new TextEditWithOffsets(3, 4, "_")) // expected additional edits
+        ),
+        // [__] [!_!] |__|
+        Arguments.of(
+            3, 4, // start, end
+            new TextRange(2, 3), // caret
+            List.of(
+                new TextEditWithOffsets(2, 3, "_"),
+                new TextEditWithOffsets(0, 1, "_")), // textEdits
+            "_3", // merged string
             Set.of(
                 new TextEditWithOffsets(2, 3, ""),
-                new TextEditWithOffsets(5, 8, "")) // expected additional edits
+                new TextEditWithOffsets(0, 1, "_")) // expected additional edits
         ),
-        // ! [__|]__[|__]
+        // [!_] [_!] |__|
+        Arguments.of(
+            5, 6, // start, end
+            new TextRange(0, 2), // caret
+            List.of(
+                new TextEditWithOffsets(1, 2, "_"),
+                new TextEditWithOffsets(2, 3, "_")
+                ), // textEdits
+            "__345", // merged string
+            Set.of(
+                new TextEditWithOffsets(1, 5, "")) // expected additional edits
+        ),
+        // [!_] [__] [_!] |__|
+        Arguments.of(
+            7, 8, // start, end
+            new TextRange(0, 5), // caret
+            List.of(
+                new TextEditWithOffsets(1, 2, "_"),
+                new TextEditWithOffsets(3, 4, "_"),
+                new TextEditWithOffsets(5, 6, "_")
+            ), // textEdits
+            "_2_4_67", // merged string
+            Set.of(
+                new TextEditWithOffsets(1, 7, "")) // expected additional edits
+        ),
+        // [!_!] [__]  |__|
+        Arguments.of(
+            7, 8, // start, end
+            new TextRange(0, 2), // caret
+            List.of(
+                new TextEditWithOffsets(0, 1, "__"),
+                new TextEditWithOffsets(4, 5, "")
+            ), // textEdits
+            "__123567", // merged string
+            Set.of(
+                new TextEditWithOffsets(0, 7, "")) // expected additional edits
+        ),
+        // [__|]!_![|__]
         Arguments.of(
             3, 5, // start, end
-            0, // caret
+            new TextRange(3, 5), // caret
             List.of(
-                new TextEditWithOffsets(2, 4, ""),
-                new TextEditWithOffsets(4, 6, "___")
+                new TextEditWithOffsets(2, 4, "__"),
+                new TextEditWithOffsets(4, 6, "__")
             ), // textEdits
-            "$001___", // merged string
+            "____", // merged string
             Set.of(
-                new TextEditWithOffsets(0, 3, ""),
+                new TextEditWithOffsets(2, 3, ""),
                 new TextEditWithOffsets(5, 6, "")) // expected additional edits
         )
     );
@@ -136,18 +160,19 @@ public class TextEditRearrangerTest {
   @MethodSource("provideAllCasesForMerge")
   public void testMergeTextEditsFromMainRangeToCaret(int replaceElementStartOffset,
                                                      int replaceElementEndOffset,
-                                                     int caretOffsetAfterInsert,
-                                                     List<TextEditWithOffsets> diffRangesAsOffsetsList,
-                                                     String expectedString,
-                                                     Set<TextEditWithOffsets> expectedAdditionalEdits) {
+                                                     @NotNull TextRange snippetBounds,
+                                                     @NotNull List<TextEditWithOffsets> diffRangesAsOffsetsList,
+                                                     @NotNull String expectedString,
+                                                     @NotNull Set<TextEditWithOffsets> expectedAdditionalEdits) {
 
-//    var res = TextEditRearranger.findOverlappingTextEditsInRangeFromMainTextEditToCaretAndMergeThem(
-//        diffRangesAsOffsetsList,
-//        replaceElementStartOffset,
-//        replaceElementEndOffset,
-//        simpleSampleText,
-//        caretOffsetAfterInsert);
-//    Assertions.assertEquals(expectedString, res.mainEdit().getNewText());
-//    Assertions.assertEquals(expectedAdditionalEdits, new HashSet<>(res.additionalEdits()));
+    var res = TextEditRearranger.findOverlappingTextEditsInRangeFromMainTextEditToCaretAndMergeThem(
+        diffRangesAsOffsetsList,
+        replaceElementStartOffset,
+        replaceElementEndOffset,
+        simpleSampleText,
+        snippetBounds
+        );
+    Assertions.assertEquals(expectedString, res.mainEdit().getNewText());
+    Assertions.assertEquals(expectedAdditionalEdits, new HashSet<>(res.additionalEdits()));
   }
 }
