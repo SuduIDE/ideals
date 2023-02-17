85283d3c47c01a8bf2fcbf5c4ed7f6fda2a4ce3d
rmzs0711 <rmzs0711@yandex.ru>
2023-02-17 13:38:23 +0100

    make base class abstract

--- server/src/test/java/org/rri/ideals/server/LspLightBasePlatformTestCase.java
+++ server/src/test/java/org/rri/ideals/server/LspLightBasePlatformTestCase.java
@@ -2,7 +2,7 @@ package org.rri.ideals.server;
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 
-public class LspLightBasePlatformTestCase extends BasePlatformTestCase {
+public abstract class LspLightBasePlatformTestCase extends BasePlatformTestCase {
   @Override
   protected boolean isIconRequired() {
     return true;
